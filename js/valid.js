a4be96c437db79a8f56cfb459e587d5450fcd6a7
Dayana <dayana.azzam@galactech.io>
2022-05-09 10:56:42 +0300

    display error message

--- js/valid.js
+++ js/valid.js
@@ -13,8 +13,19 @@ window.onload = () => {
         let isPhoneValid = isValidPhone(phone);
 
         if (!isNameValid) {
+            document.querySelector("#errorMessage").style.display='block';
             document.querySelector("#errorMessage").innerHTML = "Invalid Name"
         }
+        else if(!isEmailValid){
+            document.querySelector("#errorMessage").style.display='block';
+            document.querySelector("#errorMessage").innerHTML = "Invalid Email"
+        }
+        else if(!isPhoneValid){
+            document.querySelector("#errorMessage").style.display='block';
+            document.querySelector("#errorMessage").innerHTML = "Invalid Phone number"
+        }
+        else
+        document.querySelector("#errorMessage").style.display='none';
 
         console.log([isNameValid, isEmailValid, isPhoneValid]);
         // checkPhone(document.form1.phone);
